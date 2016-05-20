0c6e1ab29aca9a0405b2474daaf1160103153891
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-05-20 20:44:50 +0200

    add non-unique typed vertex index implementation

--- src/main/java/com/bio4j/angulillos/titan/TitanTypedVertexIndex.java
+++ src/main/java/com/bio4j/angulillos/titan/TitanTypedVertexIndex.java
@@ -64,6 +64,58 @@ public class TitanTypedVertexIndex {
       );
     }
   }
+
+  public static class NonUnique<
+    V  extends      TypedVertex<V,VT, ?, TitanVertex,TitanEdge>,
+    VT extends TypedVertex.Type<V,VT, ?, TitanVertex,TitanEdge>,
+    P extends Property<VT,X>, X
+  >
+  implements TypedVertexIndex.NonUnique<V,VT,P,X>
+  {
+
+    private final P property;
+    private final TitanUntypedGraph titanUntypedGraph;
+
+    public NonUnique(P property, TitanUntypedGraph titanUntypedGraph) {
+
+      this.property           = property;
+      this.titanUntypedGraph  = titanUntypedGraph;
+    }
+
+    public P property() { return property; }
+    public TitanUntypedGraph titanUntypedGraph() { return titanUntypedGraph; }
+
+    @Override
+    public final String name() { return "index.unique"+property._label(); }
+
+    @Override
+    public Stream<V> query(QueryPredicate.Compare predicate, X value) {
+
+      return stream(
+        titanUntypedGraph().titanGraph()
+          .query()
+          .has( "label", property().elementType()._label() )
+          .has( property()._label(), TitanConversions.Predicate.asTitanCmp(predicate), value )
+          .vertices()
+      ).map( v ->
+        property().elementType().fromRaw( (TitanVertex) v )
+      );
+    }
+
+    @Override
+    public Stream<V> query(QueryPredicate.Contain predicate, Collection<X> values) {
+
+      return stream(
+        titanUntypedGraph().titanGraph()
+          .query()
+          .has( "label", property().elementType()._label() )
+          .has( property()._label(), TitanConversions.Predicate.asTitanContain(predicate), values )
+          .vertices()
+      ).map( v ->
+        property().elementType().fromRaw( (TitanVertex) v )
+      );
+    }
+  }
 }
 
 //
